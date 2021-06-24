ab6c06c2ba13b4db025d9971c4134bd105632810
John <coldlombax@gmail.com>
2021-06-24 18:54:22 +0100

    Toggle Item now complete with custom styling based on object key-values passed.

--- src/components/Navbar/ToggleItem.jsx
+++ src/components/Navbar/ToggleItem.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-function ToggleItem() {
+function ToggleItem({ title, isLine, isButton }) {
   return (
     <li>
-      {/* {itemName} */}
+      {isLine ? <p className={`p-4 ${isButton ? 'bg-cyan-base rounded-full' : ''}`}>{title}</p> : <div className="w-full h-px bg-custom-gray mt-3 mb-3" />}
     </li>
   );
 }
 
 ToggleItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  isLine: PropTypes.bool,
+  isButton: PropTypes.bool,
+};
 
+ToggleItem.defaultProps = {
+  isLine: false,
+  isButton: false,
 };
 
 export default ToggleItem;
