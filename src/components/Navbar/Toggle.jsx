4e1406c57fd921dabe212c1b4d5baaa740c6281d
John <coldlombax@gmail.com>
2021-06-24 18:53:56 +0100

    Moved elements into array of objects.

--- src/components/Navbar/Toggle.jsx
+++ src/components/Navbar/Toggle.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ToggleItem from './ToggleItem';
+
 // eslint-disable-next-line no-unused-vars
 function Toggle({ isActive }) {
+  const navItems = [
+    { title: 'Features' },
+    { title: 'Pricing' },
+    { title: 'Resources' },
+    { title: '', isLine: true },
+    { title: 'Login' },
+    { title: 'Sign Up', isButton: true },
+  ];
+  // eslint-disable-next-line max-len
+  const navElements = navItems.map((item) => <ToggleItem title={item.title} isLine={!item.isLine} isButton={item.isButton} />);
   return (
     <div>
       {isActive && (
-      <div className="rounded-xl bg-indigo-900 w-3/4 m-auto text-white text-center p-5">
+      <div className="rounded-xl bg-purple-base w-3/4 m-auto text-white text-center p-5">
         <ul>
-          <li>
-            {/* MOVE THIS ITEM INTO IT'S OWN COMPONENT */}
-          </li>
+          {navElements}
         </ul>
       </div>
       )}
