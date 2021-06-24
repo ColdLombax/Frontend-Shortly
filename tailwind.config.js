3fc2a0484581f7f7cfb767e5592d0817fa191a0e
John <coldlombax@gmail.com>
2021-06-24 18:52:57 +0100

    Extended base colors on base theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-base': '#3A3054',
+        'cyan-base': '#2BD0D0',
+        'custom-gray': '#9E9AA8',
+      },
+    },
   },
   variants: {
     extend: {},
