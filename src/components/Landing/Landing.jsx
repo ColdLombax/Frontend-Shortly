1607ff674951dcd32b82ab67f654fe2bd241af7d
John <coldlombax@gmail.com>
2021-06-25 16:36:46 +0100

    Landing section added with main section component.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import working from '../../assets/illustration-working.svg';
+import GetStarted from './GetStarted';
 
 function Landing() {
   return (
     <div>
-      <img src={working} alt="Illustration of someone working at a desk" className="ml-10" />
+      <div className="overflow-hidden">
+        <img src={working} alt="Illustration of someone working at a desk" className="relative left-16" />
+      </div>
+      <GetStarted />
     </div>
   );
 }
