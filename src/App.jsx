d94fc5846f3363befd9404919dbfb6f0836706de
John <coldlombax@gmail.com>
2021-06-25 16:36:32 +0100

    Landing added to main component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Navbar from './components/Navbar/Navbar';
+import Landing from './components/Landing/Landing';
 
 // IMAGES
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div id="base">
       <Navbar />
+      <Landing />
     </div>
   );
 }
