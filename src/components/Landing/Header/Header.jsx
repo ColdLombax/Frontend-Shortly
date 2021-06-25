f6057900721c290eb71c0471777fb72a9eafd57f
John <coldlombax@gmail.com>
2021-06-25 17:58:52 +0100

    New header with landing details.

---
+++ src/components/Landing/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1 className="text-custom-black text-4xl text-center mt-8 mb-5">More than just shorter links</h1>
+      <p className="text-center m-auto w-5/6">
+        Build your brand’s recognition and get detailed
+        insights on how your links are performing.
+      </p>
+      <div className="flex justify-center mt-5">
+        <button type="button" className="pr-10 pl-10 p-3 rounded-full bg-cyan-base text-center text-white">Get Started</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
