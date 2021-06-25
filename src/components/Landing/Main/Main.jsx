adf360b81556dbe178a7f8181afee4a1643d470f
John <coldlombax@gmail.com>
2021-06-25 23:10:59 +0100

    Link List array of objects added, cards return multiple card components.

--- src/components/Landing/Main/Main.jsx
+++ src/components/Landing/Main/Main.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Input from './Input';
+import LinkCard from './LinkCard';
 
 function Main() {
+  const [linkList] = useState([
+    { short: 'short link', long: 'long link' },
+    { short: 'test link', long: 'long link' },
+  ]);
+
+  const cards = linkList.map((link) => <LinkCard short={link.short} long={link.long} />);
+
   return (
     <main>
       <div className="flex justify-center mt-20">
         <Input />
       </div>
+      <section id="cards">
+        {cards}
+      </section>
     </main>
   );
 }
