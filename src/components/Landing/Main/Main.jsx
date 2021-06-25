6adcb6e389c7c2b8c526707785d1aadb5e26e74e
John <coldlombax@gmail.com>
2021-06-25 17:59:25 +0100

    Main section added, including input component.

---
+++ src/components/Landing/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Input from './Input';
+
+function Main() {
+  return (
+    <main>
+      <div className="flex justify-center mt-20">
+        <Input />
+      </div>
+    </main>
+  );
+}
+
+export default Main;
