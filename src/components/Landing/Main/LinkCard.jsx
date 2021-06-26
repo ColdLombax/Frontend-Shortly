47047f245457d24042c1f5c834ce48892ef7e66a
John <coldlombax@gmail.com>
2021-06-26 13:28:08 +0100

    Link cards now correctly styled and displays without overflow.

--- src/components/Landing/Main/LinkCard.jsx
+++ src/components/Landing/Main/LinkCard.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function LinkCard({ short, long }) {
+  const linkOverflowStyle = 'pb-2 pt-2 overflow-ellipsis overflow-hidden whitespace-nowrap';
   return (
-    <div className="m-auto w-3/4 bg-cyan-base">
-      <p>{long}</p>
-      <p>{short}</p>
-      <button type="button">Copy</button>
+    <div className="m-auto w-5/6 mb-5 bg-white rounded-md text-custom-black p-4">
+      <p className={`${linkOverflowStyle}`}>{long}</p>
+      <hr />
+      <p className={`${linkOverflowStyle} text-cyan-base`}>{short}</p>
+      <button type="button" className="bg-cyan-base w-full rounded-sm text-white p-2 font-bold">Copy</button>
     </div>
   );
 }
