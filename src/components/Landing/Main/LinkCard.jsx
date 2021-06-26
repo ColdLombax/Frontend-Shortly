a7a160e3100762ff836bcfe2e8b00fb1f51badc6
John <coldlombax@gmail.com>
2021-06-26 15:05:28 +0100

    Copy to clipboard working, indication is required.

--- src/components/Landing/Main/LinkCard.jsx
+++ src/components/Landing/Main/LinkCard.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function LinkCard({ short, long }) {
+  const copyHandler = () => {
+    const temp = document.createElement('textarea');
+    temp.value = short;
+    document.body.appendChild(temp);
+    temp.select();
+    document.execCommand('copy');
+    document.body.removeChild(temp);
+  };
+
+  // Paragraph Styling
   const linkOverflowStyle = 'pb-2 pt-2 overflow-ellipsis overflow-hidden whitespace-nowrap';
   return (
     <div className="m-auto w-5/6 mb-5 bg-white rounded-md text-custom-black p-4">
       <p className={`${linkOverflowStyle}`}>{long}</p>
       <hr />
       <p className={`${linkOverflowStyle} text-cyan-base`}>{short}</p>
-      <button type="button" className="bg-cyan-base w-full rounded-sm text-white p-2 font-bold">Copy</button>
+      <button type="button" onClick={copyHandler} className="bg-cyan-base w-full rounded-sm text-white p-2 font-bold">Copy</button>
     </div>
   );
 }
