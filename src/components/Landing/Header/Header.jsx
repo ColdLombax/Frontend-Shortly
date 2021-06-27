46d36f30a7edca3907d13571ca7c5f614a25adc6
John <coldlombax@gmail.com>
2021-06-27 22:55:20 +0100

    Font weight changed to bold.

--- src/components/Landing/Header/Header.jsx
+++ src/components/Landing/Header/Header.jsx
@@ -5,13 +5,13 @@ function Header() {
   const focusStyle = useContext(UserContext);
   return (
     <header>
-      <h1 className="text-custom-black text-4xl text-center mt-8 mb-5">More than just shorter links</h1>
+      <h1 className="text-custom-black text-4xl text-center mt-8 mb-5 font-bold">More than just shorter links</h1>
       <p className="text-center m-auto w-5/6">
         Build your brand’s recognition and get detailed
         insights on how your links are performing.
       </p>
       <div className="flex justify-center mt-5">
-        <button type="button" className={`pr-10 pl-10 p-3 rounded-full bg-cyan-base text-center text-white ${focusStyle}`}>Get Started</button>
+        <button type="button" className={`pr-10 pl-10 p-3 rounded-full bg-cyan-base text-center text-white ${focusStyle} font-bold`}>Get Started</button>
       </div>
     </header>
   );
