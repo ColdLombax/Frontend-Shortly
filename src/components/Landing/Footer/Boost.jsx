e922bfc9008b5744d6227bee17546865cb461a07
John <coldlombax@gmail.com>
2021-06-29 00:22:24 +0100

    Boost complete with styling and with button and focus.

---
+++ src/components/Landing/Footer/Boost.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+
+import UserContext from '../../../UserContext';
+// import PropTypes from 'prop-types';
+
+function Boost() {
+  const focusStyle = useContext(UserContext);
+  return (
+    <section className="bg-purple-base mt-16 flex flex-col items-center bg-mobile-boost-bg bg-no-repeat bg-cover h-60 justify-center">
+      <h2 className="text-white text-bold text-3xl font-bold">Boost your links today</h2>
+      <button
+        type="submit"
+        className={`bg-cyan-base pr-10 pl-10 p-4 rounded-full text-xl text-white font-bold mt-5 ${focusStyle}`}
+      >
+        Get Started
+      </button>
+    </section>
+  );
+}
+
+Boost.propTypes = {
+
+};
+
+export default Boost;
