6cf992124df781a36cb5858bc4ac49987ddd261a
John <coldlombax@gmail.com>
2021-06-29 00:21:44 +0100

    Social section added without anchor links.

---
+++ src/components/Landing/Footer/Social.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import facebook from '../../../assets/icon-facebook.svg';
+import twitter from '../../../assets/icon-twitter.svg';
+import pinterest from '../../../assets/icon-pinterest.svg';
+import instagram from '../../../assets/icon-instagram.svg';
+
+function Social() {
+  return (
+    <ul className="flex gap-5 mt-10">
+      <li>
+        <img src={facebook} alt="facebook" />
+      </li>
+      <li>
+        <img src={twitter} alt="facebook" />
+      </li>
+      <li>
+        <img src={pinterest} alt="facebook" />
+      </li>
+      <li>
+        <img src={instagram} alt="facebook" />
+      </li>
+    </ul>
+  );
+}
+
+export default Social;
