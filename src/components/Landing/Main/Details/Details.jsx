a14d83602d777a5d0e7d785fb5f38737c1b15bac
John <coldlombax@gmail.com>
2021-06-28 23:50:38 +0100

    Changed icon label on third object for info data.

--- src/components/Landing/Main/Details/Details.jsx
+++ src/components/Landing/Main/Details/Details.jsx
@@ -6,7 +6,7 @@ function Details() {
   const infoData = [
     { icon: 'recognition', title: 'Brand Recognition', desc: 'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.' },
     { icon: 'records', title: 'Detailed Records', desc: 'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.' },
-    { icon: 'customize', title: 'Fully Customizable', desc: 'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.' },
+    { icon: 'customizeable', title: 'Fully Customizable', desc: 'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.' },
   ];
 
   // eslint-disable-next-line max-len
