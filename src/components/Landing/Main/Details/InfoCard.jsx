428184154ddd9db4287c159b4162484a49252d83
John <coldlombax@gmail.com>
2021-06-28 23:50:26 +0100

    Stlying and switching icons for details.

--- src/components/Landing/Main/Details/InfoCard.jsx
+++ src/components/Landing/Main/Details/InfoCard.jsx
@@ -1,12 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import recognition from '../../../../assets/icon-brand-recognition.svg';
+import records from '../../../../assets/icon-detailed-records.svg';
+import customizeable from '../../../../assets/icon-fully-customizable.svg';
+
 function InfoCard({ icon, title, desc }) {
+  let iconPick = recognition;
+  switch (icon) {
+    case 'records':
+      iconPick = records;
+      break;
+    case 'customizeable':
+      iconPick = customizeable;
+      break;
+    default:
+      iconPick = recognition;
+      break;
+  }
   return (
-    <section className="mt-10 m-auto w-5/6">
-      <p className="">{icon}</p>
-      <p>{title}</p>
-      <p>{desc}</p>
+    <section className="mt-20 m-auto w-5/6 bg-white rounded-md p-8 relative">
+      <div className="w-full flex justify-center absolute -top-10 -left-0">
+        <img src={iconPick} alt={title} className="bg-purple-base p-5 rounded-full" />
+      </div>
+      <p className="text-custom-black font-bold text-xl mt-5">{title}</p>
+      <p className="text-sm mt-5">{desc}</p>
     </section>
   );
 }
