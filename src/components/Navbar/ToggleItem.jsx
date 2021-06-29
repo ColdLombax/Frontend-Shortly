aabd6e9360e64b94f7057e642e989fe0bbfd1801
John <coldlombax@gmail.com>
2021-06-29 11:01:26 +0100

    Hover state added to toggle button, with pointers.

--- src/components/Navbar/ToggleItem.jsx
+++ src/components/Navbar/ToggleItem.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 function ToggleItem({ title, isLine, isButton }) {
   return (
-    <li className={`${title === 'Resources' ? 'mr-auto' : ''}`}>
-      {isLine ? <p className={`p-2 lg:pr-5 lg:pl-5 ${isButton ? 'bg-cyan-base rounded-full text-white font-bold' : ''}`}>{title}</p> : <div className="w-full h-px bg-custom-gray mt-3 mb-3" />}
+    <li className={`${title === 'Resources' ? 'mr-auto' : ''} lg:hover:text-custom-black lg:cursor-pointer`}>
+      {isLine ? <p className={`p-2 lg:pr-5 lg:pl-5 ${isButton ? 'bg-cyan-base rounded-full text-white font-bold lg:hover:bg-cyan-hover' : ''}`}>{title}</p> : <div className="w-full h-px bg-custom-gray mt-3 mb-3" />}
     </li>
   );
 }
