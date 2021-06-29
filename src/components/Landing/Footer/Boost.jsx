1cc6b24a101af2969120b2e83e532194c46568bc
John <coldlombax@gmail.com>
2021-06-29 13:27:30 +0100

    Padding expanded & hover state for button.

--- src/components/Landing/Footer/Boost.jsx
+++ src/components/Landing/Footer/Boost.jsx
@@ -6,11 +6,11 @@ import UserContext from '../../../UserContext';
 function Boost() {
   const focusStyle = useContext(UserContext);
   return (
-    <section className="bg-purple-base mt-16 flex flex-col items-center bg-mobile-boost-bg bg-no-repeat bg-cover h-60 justify-center">
-      <h2 className="text-white text-bold text-3xl font-bold">Boost your links today</h2>
+    <section className="bg-purple-base mt-16 flex flex-col items-center bg-mobile-boost-bg bg-no-repeat bg-cover h-60 justify-center lg:p-20">
+      <h2 className="text-white text-bold text-3xl font-bold lg:text-4xl">Boost your links today</h2>
       <button
         type="submit"
-        className={`bg-cyan-base pr-10 pl-10 p-4 rounded-full text-xl text-white font-bold mt-5 ${focusStyle}`}
+        className={`bg-cyan-base pr-10 pl-10 p-4 rounded-full text-xl text-white font-bold mt-5 ${focusStyle} lg:hover:bg-cyan-hover`}
       >
         Get Started
       </button>
